b44a4cdb6efb2a26ea6321a9dfe46fcdf04287c7
Sahar Shayegan <saharOmidiShayegan@gmail.com>
2020-12-03 23:27:49 +0330

    Complete add post API.

--- server/routes/api/admin.js
+++ server/routes/api/admin.js
@@ -4,24 +4,25 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 
-router.post('/', async (req, res) => {
+router.post('/post', async (req, res) => {
 	try{
-    const db = getDB();
-    db.createCollection("employees", function(err, res) {
-      if (err) throw err;
-      console.log("Collection is created!");
-      db.close();
+    const {text, adminId} = req.body;
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+      adminId,
+      text,
     });
-	  res.status(201).send();
+	  res.status(201).send({'result': 'ok'});
   } catch(err){
 	   console.log(err)
+     res.send(err);
   }
 });
 
-async function getDB() {
+async function loadPostsCollection() {
   const MongoClient = await mongodb.MongoClient.connect
   ("mongodb+srv://pxrsw:B123123b@habittracker.zsjy4.mongodb.net/habitTracker?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
-  return MongoClient.db('habitTracker');
+  return MongoClient.db('habitTracker').collection('posts');
 }
 
 module.exports = router;
